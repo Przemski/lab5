852c0ccc18e1efec8f90a662ed226906d005de75
Przemski <48888990+Przemski@users.noreply.github.com>
2019-03-24 18:44:50 +0100

    Update Program.java

--- Program.java
+++ Program.java
@@ -1,8 +1,12 @@
 public class Program {
-public static void main(String[] args) { Punkt p = new Punkt();
+public static void main(String[] args) { 
+Punkt p = new Punkt();
 p.set();
 System.out.println(p.wsp());
 
+Odcinek o = new Odcinek();
+o.set();
+System.out.println(o.dl());
 
 
 Okrag oo = new Okrag(2);
