a790bdf996130bd446fd092110d375a6e2fda80d
Przemski <48888990+Przemski@users.noreply.github.com>
2019-03-24 18:39:22 +0100

    Update Odcinek.java

--- Odcinek.java
+++ Odcinek.java
@@ -1,10 +1,52 @@
+import java.util.Scanner;
+
 public class Odcinek {
 private Punkt p1 = new Punkt();
 private Punkt p2 = new Punkt();
+private Scanner ok = new Scanner(System.in);
 Odcinek(double x1,double y1,double x2,double y2) {
 p1.set(x1,y1);
 p2.set(x2,y2);
 }
+
+
+public Punkt getP1() {
+	return p1;
+}
+
+
+public void setP1(Punkt p1) {
+	this.p1 = p1;
+}
+
+
+public Punkt getP2() {
+	return p2;
+}
+
+
+public void setP2(Punkt p2) {
+	this.p2 = p2;
+}
+
+public void set (double x1,double y1,double x2,double y2) {
+this.x1=x1;
+this.x2=x2;
+this.y1=y1;
+this.y2=y2;
+}
+
+public void set() {
+System.out.print("x1=");
+x1=ok.nextDouble();
+System.out.print("y1=");
+y1=ok.nextDouble();
+System.out.print("x2=");
+x2=ok.nextDouble();
+System.out.print("y2=");
+y2=ok.nextDouble();
+}
+
 public String wsp() {
 return this.p1.wsp()+this.p2.wsp();
 }
